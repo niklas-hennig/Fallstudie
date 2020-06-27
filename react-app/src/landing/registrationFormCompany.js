64bf5666f692e5393dcab7e6e2db5c4c9574d11c
Dennis <den.bur@outlook.de>
2020-06-27 21:14:24 +0200

    ip auf localhost geändert in registration company

--- react-app/src/landing/registrationFormCompany.js
+++ react-app/src/landing/registrationFormCompany.js
@@ -29,7 +29,7 @@ class RegistrationForm extends Component {
     submitHandler = (event) => {
         event.preventDefault();
         if (this.state.registration === true) {
-            axios.post('http://192.168.0.14/api/Company', {
+            axios.post('http://localhost/api/Company', {
                 name: this.state.name,
                 street: this.state.street,
                 number: this.state.number,
@@ -53,7 +53,7 @@ class RegistrationForm extends Component {
 
     searchHandler = (event) => {
         event.preventDefault();
-        axios.get('http://192.168.0.14/api/Company/Existence/' + this.state.name)
+        axios.get('http://localhost/api/Company/Existence/' + this.state.name)
             .then((res) => {
                 if (res.data) {
                     this.props.onRegistered(res.data.name);
